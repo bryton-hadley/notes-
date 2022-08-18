531f9991228604644b4d9c174fe33dad57535222
Bryton Haldey <bryton.hadley@gmail.com>
2022-08-18 12:14:41 -0600

    1st commit

---
+++ notes.js
@@ -0,0 +1,40 @@
+//let myArray = ['Adam', 'Gabe', 'Lane', 'Bryton', 'Ty']
+// to grabe an element from the array you would console log and call it from its spot in the line console.log(myArray[0])
+
+// you can push as many things as you want just make sure to put commas ,
+//for example//
+//myArray.push('Kelsey')
+// remove and ideam from your list
+//myArray.pop()
+// if you want to remove an item from your array and add it to the line use this method 
+//let poppedItem = myArray.pop()
+// this will pull it from the start 
+//let poppedItem = myArray.shift()
+//allows you to add something to the being of the array 
+//myArray.unshift('Kelsey')
+// .slice() makes a shllow copy with two aruments examp, let slicedArray = myArray.slice(0,3)
+// .splice() is most dynamic of all eadting arrays. it takes 3 arguments 
+//let splicedArray = 
+//  myArray.splice(2,1, 'James', 'Toky', )
+
+//console.log(copiedArray)
+//console.log(myArray)
+// we are able to use loops to dynamically look at each item in an array based index 
+//lets take a look 
+// examp 1
+// let myArray = ['Adam', 'Gabe', 'Lane', 'Kelsey', 'Bryton', 'Ty']
+// for(let i = 0; i < myArray.length; i++){
+//   console.log(i)
+//   console.log(myArray[i])
+// }
+// //examp 2
+// let orderPrices = [4, 2, 7,2, 78,3]
+
+// let total = 0
+
+// for(let i = 0; i < orderPrices.length; i++){
+//   // console.log(i)
+//   total += orderPrices[i]
+//   console.log(total)
+// }
+
